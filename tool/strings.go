1f529841fddf7625d91c8cafc7b6d1fa100533b6
dingdinglz <1149978856@qq.com>
2025-05-19 10:26:46 +0800

    🐞 fix(tool): 修复markdown两边不闭合的问题

--- tool/strings.go
+++ tool/strings.go
@@ -10,6 +10,9 @@ func StringBetweenContain(s string, first string, end string) string {
 	if firstIndex == -1 || endIndex == -1 {
 		return s
 	}
+	if firstIndex >= endIndex+len(end) {
+		return s
+	}
 	return s[firstIndex : endIndex+len(end)]
 }
 
@@ -17,6 +20,9 @@ func StringBetween(s string, first string, end string) string {
 	firstIndex := strings.Index(s, first)
 	endIndex := strings.LastIndex(s, end)
 	if firstIndex == -1 || endIndex == -1 {
+		return s
+	}
+	if firstIndex+len(first) >= endIndex {
 		return s
 	}
 	return s[firstIndex+len(first) : endIndex]
